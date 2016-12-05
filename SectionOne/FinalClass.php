8215822a0da24e1354a08d9fabb7661ea761939d
chendaye <chendaye666@gmail.com>
2016-12-05 21:13:33 +0800

    final 关键字

---
+++ SectionOne/FinalClass.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * 如果希望类和方法完成确定不变的功能，不希望被继承或者被覆盖
+     * 就可以用final关键字来限定
+     */
+    final class Play{
+        //TODO:some thing
+
+    }
+    class Go {
+        final public function run(){
+            //TODO:some thing
+        }
+    }
+?>
